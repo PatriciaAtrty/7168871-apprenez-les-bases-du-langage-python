9e995eccd6eb851e556d48f21d9d467d99fb5078
PatriciaAtrty <patricia.atry@gmail.com>
2025-02-13 17:47:17 +0100

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+#Créez une variable nommée `nom` et attribuez-y votre **nom**
+nom(patricia)
+#Créez une variable nommée `age` et attribuez-y votre **âge**
+age(57)
+#Affichez une phrase contenant votre **nom** et votre **âge** en utilisant les variables que vous venez de créer
+"Je m'appelle" nom()  et j'ai age() ans."
+#Modifiez la valeur de la variable age pour ajouter **10 ans** à votre **âge** actuel
+age1(age+10)
+#Affichez à nouveau la phrase contenant votre **nom** et votre **âge** en utilisant les variables mises à jour
+"Je m'appelle nom() et j'ai age1() ans maintenant."
