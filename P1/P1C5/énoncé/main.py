5a71b186d13e17b47c99a879a67a712c0e709334
PatriciaAtrty <patricia.atry@gmail.com>
2025-02-13 22:41:58 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,15 @@
-# Écrivez votre code ici !
+# Définissez une variable `nom` contenant votre **nom** sous forme de chaîne de caractères.Définissez une variable `age` contenant votre **âge** sous forme d'un entier.Définissez une variable `taille` contenant votre **taille** sous forme d'un nombre à virgule flottante.Définissez une variable `est_etudiant` contenant une valeur booléenne et indiquant si vous êtes **étudiant ou non**.
+nom = "patricia"
+age = 57
+taille=1.62
+est_etudiant=true
+#Utilisez la fonction `print` pour afficher les valeurs des variables précédemment définies.
+print(f"{nom}")
+print(f"{age}")
+print(f"{taille}")
+print(f"{est_etudiant}")
+# Utilisez la fonction `type` pour afficher avec la fonction `print` les types des variables précédemment définies.
+print(f"Type nom:{type(nom)}")
+print(f"Type age:{type(age)}")
+print(f"Type taille:{type(taille)}")
+print(f"Type est etudiant:{type(est_etudiant)}")
