da840879e393557a2266b8cb0852c86123a0b685
PatriciaAtrty <patricia.atry@gmail.com>
2025-02-14 17:28:18 +0100

    Update main.py

--- P1/P1C3/énoncé/main.py
+++ P1/P1C3/énoncé/main.py
@@ -1,1 +1,6 @@
-## Écrivez votre code ici !
+Dans cette exercice, vous allez écrire directement dans un fichier python qui se nomme `main.py`.
+
+  #1. Utilisez la fonctions `print` pour afficher la phrase `"J'apprends Python !"`
+print("j'apprends Python!")
+  #2. Calculez le résultat de `17 + 35 * 2` et affichez le résultat (Vous pouvez placer le calcul directement dans la fonction `print` pour l'afficher)
+print(17+35*2)
